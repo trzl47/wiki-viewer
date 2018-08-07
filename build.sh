996ad6f70ca22cf3755a189bbe5b6d7de912c488
trzl <brownty47@gmail.com>
2018-08-07 19:29:52 +0000

    build script now automatically pushes

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ docker system prune --volumes && \
 mkdir services/wiki-nginx/build && \
 cp -r services/react/build/client services/wiki-nginx/build && \
 docker build -t trzl/portfolio:wiki-react.1 -f ./services/wiki-nginx/build.Dockerfile ./services/wiki-nginx && \
-# docker push trzl/portfolio:wiki-react.1 && \
+docker push trzl/portfolio:wiki-react.1 && \
 docker build -t trzl/portfolio:wiki-proxy.1 -f ./services/reverseproxy/Dockerfile ./services/reverseproxy && \
-# docker push trzl/portfolio:wiki-proxy.1 && \
+docker push trzl/portfolio:wiki-proxy.1 && \
 docker-compose down && \
 docker system prune --volumes && \
 sudo rm -rf services/wiki-nginx/build && \
